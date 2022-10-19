09840860fccd0b3ac598fed78a390b6319e39826
tlaehdtlr <tlaehdtlr@gmail.com>
2022-10-19 14:08:29 +0900

    utilize alert dialog class
    Change-Id: I1aba104a692c602d645437cd185ff5355d7a86ec

--- AlertDialog/app/src/main/java/com/example/alertdialog/MainActivity.kt
+++ AlertDialog/app/src/main/java/com/example/alertdialog/MainActivity.kt
@@ -1,11 +1,95 @@
 package com.example.alertdialog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import com.example.alertdialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    val positiveButtonClick = { dialoginterface : DialogInterface, i: Int ->
+        toast("Positive")
+    }
+
+    val negativeButtonClick = { dialoginterface : DialogInterface, i: Int ->
+        toast("Negative")
+    }
+
+    val neutralButtonClick = { dialoginterface : DialogInterface, i: Int ->
+        toast("Neutral")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBasicAlertDialog.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+                .setTitle("Title")
+                .setMessage("Messaage")
+                .setPositiveButton("Positive", positiveButtonClick)
+                .setNegativeButton("Negative", negativeButtonClick)
+                .setNeutralButton("Neutral", neutralButtonClick)
+                .show()
+        }
+
+        binding.btnItemAlertDialog.setOnClickListener {
+            val items = arrayOf("Apple", "Orange", "Mango", "Lemon")
+            val builder = AlertDialog.Builder(this)
+                .setTitle("Select item")
+                .setItems(items) {dialog, which ->
+                    toast("${items[which]} is Selected")
+                }
+                .show()
+        }
+
+        binding.btnRadioAlertDialog.setOnClickListener {
+            val items = arrayOf("Apple", "Orange", "Mango", "Lemon")
+            var selectedItem:String? = null
+            val builder = AlertDialog.Builder(this)
+                .setTitle("Select Item")
+                .setSingleChoiceItems(items, -1) { dialog, which ->
+                    selectedItem = items[which]
+                }
+                .setPositiveButton("OK"){dialog, which ->
+                    toast("${selectedItem.toString()} is selected")
+                }
+                .show()
+        }
+
+        binding.btnCheckBoxAlertDialog.setOnClickListener {
+            val items = arrayOf("Apple", "Orange", "Mango", "Lemon")
+            val selectedItemIndex = ArrayList<Int>()
+
+            val builder = AlertDialog.Builder(this)
+                .setTitle("Select Items")
+                .setMultiChoiceItems(items, null) { dialoginterface: DialogInterface, i: Int, b: Boolean ->
+                    if (b) {
+                        selectedItemIndex.add(i)
+                    } else if (selectedItemIndex.contains(i)) {
+                        selectedItemIndex.remove(i)
+                    }
+                }
+                .setPositiveButton("ok"){dialoginterface:DialogInterface, i:Int ->
+                    val selected = ArrayList<String>()
+                    for(j in selectedItemIndex){
+                        selected.add(items[j])
+                    }
+                    toast("Selected items : $selected")
+                }
+                .show()
+        }
+
+    }
+
+
+    fun toast(message:String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
